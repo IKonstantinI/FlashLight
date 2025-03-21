083a01bd7b524327304855063722e9d9eee991a3
Konstantin <diel404@gmail.com>
2025-03-21 21:38:44 +0300

    Fix logic

--- FlashLight/ViewController.swift
+++ FlashLight/ViewController.swift
@@ -1,45 +1,62 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var actionButton: UIButton!
     
+    private var currentLight = CurrentLight.red
     private let lightOn = 1.0
     private let lightOff = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.layer.cornerRadius = redView.frame.height / 2
-        yellowView.layer.cornerRadius = yellowView.frame.height / 2
-        greenView.layer.cornerRadius = greenView.frame.height / 2
+        
         redView.alpha = lightOff
         yellowView.alpha = lightOff
         greenView.alpha = lightOff
-        actionButton.setTitle(NSLocalizedString("START", comment: ""), for: .normal)
+        
+        actionButton.setTitle("START", for: .normal)
         actionButton.backgroundColor = .systemBlue
-        actionButton.setTitleColor(.white, for: .normal)
+        actionButton.titleLabel?.font = .systemFont(ofSize: 17, weight: .bold)
+        actionButton.setTitleColor(.white,for: .normal)
         actionButton.layer.cornerRadius = 8
     }
     
-    @IBAction func actionButtonDidTapped(_ sender: UIButton) {
-        sender.setTitle("NEXT", for: .normal)
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+    }
+    
+    @IBAction func actionButtonDidTapped() {
+        if actionButton.currentTitle == "START" {
+            actionButton.setTitle("NEXT",for: .normal)
+        }
         
-        UIView.animate(withDuration: 0.9) {
-            if self.redView.alpha == self.lightOn {
-                self.redView.alpha = self.lightOff
-                self.yellowView.alpha = self.lightOn
-            } else if self.yellowView.alpha == self.lightOn {
-                self.yellowView.alpha = self.lightOff
-                self.greenView.alpha = self.lightOn
-            } else if self.greenView.alpha == self.lightOn {
-                self.greenView.alpha = self.lightOff
-                self.redView.alpha = self.lightOn
-            } else {
-                self.redView.alpha = self.lightOn
-            }
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightOff
+            redView.alpha = lightOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenView.alpha = lightOn
+            yellowView.alpha = lightOff
+            currentLight = .red
         }
     }
+    
 }
-
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
